963865d349bc420a38c12a11dd092f120193a907
lokesh541 <lrbeeram@gmail.com>
2016-01-25 23:55:24 +0530

    timer added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
-$(document).ready(){
-  var seconds;
-  var minutes;
-  function timeUpdate(){
-    var intervalId = setInterval(function () {
+$(document).ready(function(){
+  var seconds = 60;
+  var minutes = Number(document.getElementById("session").innerHtml);
+$("#start").click(function(){
+  console.log(minutes);
+  console.log(seconds);
+      timeUpdate();
+    });
+});
 
-    }, 1000);
-  }
+
+
+function timeUpdate(){
+if (seconds >= 0){
+ seconds--;
+}
+if (seconds == 0 && minutes >= 0) {
+  minutes--;
+}
+
+  minutes = checkTime(minutes);
+  seconds = checkTime(seconds);
+  document.getElementById("session").innerHtml =  minutes + ":" + seconds;
+  var intervalId = setInterval(timeUpdate, 1000);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
