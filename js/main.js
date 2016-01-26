7fc155f1bd30ba41d8c0af825f8d8f86eba8d79b
lokesh541 <lrbeeram@gmail.com>
2016-01-26 21:35:03 +0530

    me

--- js/main.js
+++ js/main.js
@@ -6,24 +6,22 @@ $("#start").click(function(){
   console.log(seconds);
       timeUpdate();
     });
+    function timeUpdate(){
+    if (seconds >= 0){
+     seconds--;
+    }
+    if (seconds == 0 && minutes >= 0) {
+      minutes--;
+    }
+
+      minutes = checkTime(minutes);
+      seconds = checkTime(seconds);
+      document.getElementById("session").innerHtml =  minutes + ":" + seconds;
+      var intervalId = setInterval(timeUpdate, 1000);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+        return i;
+    }
+
 });
-
-
-
-function timeUpdate(){
-if (seconds >= 0){
- seconds--;
-}
-if (seconds == 0 && minutes >= 0) {
-  minutes--;
-}
-
-  minutes = checkTime(minutes);
-  seconds = checkTime(seconds);
-  document.getElementById("session").innerHtml =  minutes + ":" + seconds;
-  var intervalId = setInterval(timeUpdate, 1000);
-}
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
